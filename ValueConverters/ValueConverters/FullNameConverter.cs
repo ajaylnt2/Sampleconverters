2045a6acd923d3db0bcd95f180a5ef9d1f701080
Ajay Kumar  Kosuru <AjayKumar.Kosuru_ext@Lnttechservices.com>
2017-11-23 18:28:06 +0530

    wpf converters

---
+++ ValueConverters/ValueConverters/FullNameConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ValueConverters
+{
+    public class FullNameConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (values != null)
+            {
+                return values[0] + " " + values[1];
+            }
+            return " ";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string[] values=null;
+            if (value != null)
+                return values = value.ToString().Split(' ');
+            return values;
+        }
+    }
+}
